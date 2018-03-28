5321333cd15b730ec3658122407d6281a8f42d81
AeronTor <aarontornes@gmail.com>
2018-03-27 20:21:10 -0400

    Added comments to app.js

--- Js/app.js
+++ Js/app.js
@@ -1,7 +1,10 @@
+// Creating a modal program here
+
 var modal = document.querySelector(".modal");
 var openButton = document.querySelector(".open-button");
 var closeButton = document.querySelector(".close-button");
 
+// toggleModal changes modal's class to "reveal" which has CSS property visibility: visible.  Original class of modal ("modal") visibility is set to hidden
 function toggleModal () {
     modal.classList.toggle("reveal");
 }
